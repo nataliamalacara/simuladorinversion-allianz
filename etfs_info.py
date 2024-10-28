5eae2192da54a17e5ada077bae39d1e9cb036ebc
nataliamalacara <0233201@up.edu.mx>
2024-10-28 11:34:41 -0600

    Subida inicial del simulador

---
+++ etfs_info.py
@@ -0,0 +1,102 @@
+ETFs_Data = [
+    {
+        "nombre": "AZ QQQ NASDAQ 100",
+        "descripcion": "ETF que sigue el rendimiento del índice NASDAQ 100",
+        "simbolo": "QQQ"
+    },
+    {
+        "nombre": "AZ SPDR S&P 500 ETF TRUST",
+        "descripcion": "ETF que sigue el rendimiento del índice S&P 500",
+        "simbolo": "SPY"
+    },
+    {
+        "nombre": "AZ SPDR DJIA TRUST",
+        "descripcion": "ETF que sigue el rendimiento del índice Dow Jones Industrial Average",
+        "simbolo": "DIA"
+    },
+    {
+        "nombre": "AZ VANGUARD EMERGING MARKET ETF",
+        "descripcion": "ETF de Vanguard que sigue el rendimiento de mercados emergentes",
+        "simbolo": "VWO"
+    },
+    {
+        "nombre": "AZ FINANCIAL SELECT SECTOR SPDR",
+        "descripcion": "ETF que sigue el rendimiento del sector financiero de EE.UU.",
+        "simbolo": "XLF"
+    },
+    {
+        "nombre": "AZ HEALTH CARE SELECT SECTOR",
+        "descripcion": "ETF que sigue el rendimiento del sector de salud de EE.UU.",
+        "simbolo": "XLV"
+    },
+    {
+        "nombre": "AZ DJ US HOME CONSTRUCT",
+        "descripcion": "ETF que sigue el rendimiento del sector de construcción en EE.UU.",
+        "simbolo": "ITB"
+    },
+    {
+        "nombre": "AZ SPDR GOLD TRUST",
+        "descripcion": "ETF que sigue el rendimiento del oro",
+        "simbolo": "GLD"
+    },
+    {
+        "nombre": "AZ ISHARES MSCI CHINA",
+        "descripcion": "ETF que sigue el rendimiento del mercado chino",
+        "simbolo": "MCHI"
+    },
+    {
+        "nombre": "AZ ISHARES MSCI TAIWAN",
+        "descripcion": "ETF que sigue el rendimiento del mercado de Taiwán",
+        "simbolo": "EWT"
+    },
+    {
+        "nombre": "AZ ISHARES MSCI BRAZIL",
+        "descripcion": "ETF que sigue el rendimiento del mercado brasileño",
+        "simbolo": "EWZ"
+    },
+    {
+        "nombre": "AZ ISHARES MSCI UNITED KINGDOM",
+        "descripcion": "ETF que sigue el rendimiento del mercado del Reino Unido",
+        "simbolo": "EWU"
+    },
+    {
+        "nombre": "AZ ISHARES MSCI SOUTH KOREA",
+        "descripcion": "ETF que sigue el rendimiento del mercado de Corea del Sur",
+        "simbolo": "EWY"
+    },
+    {
+        "nombre": "AZ ISHARES MSCI EMERGING MARKETS",
+        "descripcion": "ETF que sigue el rendimiento de los mercados emergentes",
+        "simbolo": "EEM"
+    },
+    {
+        "nombre": "AZ ISHARES MSCI GERMANY",
+        "descripcion": "ETF que sigue el rendimiento del mercado alemán",
+        "simbolo": "EWG"
+    },
+    {
+        "nombre": "AZ ISHARES MSCI JAPAN",
+        "descripcion": "ETF que sigue el rendimiento del mercado japonés",
+        "simbolo": "EWJ"
+    },
+    {
+        "nombre": "AZ ISHARES MSCI HONG KONG",
+        "descripcion": "ETF que sigue el rendimiento del mercado de Hong Kong",
+        "simbolo": "EWH"
+    },
+    {
+        "nombre": "AZ RUSSELL 2000",
+        "descripcion": "ETF que sigue el rendimiento del índice Russell 2000",
+        "simbolo": "IWM"
+    },
+    {
+        "nombre": "AZ ISHARES MSCI FRANCE",
+        "descripcion": "ETF que sigue el rendimiento del mercado francés",
+        "simbolo": "EWQ"
+    },
+    {
+        "nombre": "AZ ISHARES MSCI BRIC",
+        "descripcion": "ETF que sigue el rendimiento de los mercados de Brasil, Rusia, India y China",
+        "simbolo": "BKF"
+    }
+]
